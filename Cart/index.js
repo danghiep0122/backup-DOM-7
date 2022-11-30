0bf3969f65a793aaba1e1488af29990882075fe2
Nguyen Dang Hiep <99936776+danghiep0122@users.noreply.github.com>
2022-11-30 10:57:24 +0700

    Update index.js

--- Cart/index.js
+++ Cart/index.js
@@ -20,3 +20,29 @@ function Cart() {
 }
 
 export default Cart
+
+
+// 30/11
+
+import { useSelector } from 'react-redux'
+
+import { EmptyCart } from './EmptyCart'
+import { CartItems } from './CartItems'
+import CommonLayout from '../../layouts/commonLayout/CommonLayout'
+import './styles.scss'
+
+function Cart() {
+  const cartList = useSelector((state) => state.CART_ITEM.value)
+
+  return (
+    <CommonLayout>
+      <section className="cart-page">
+        <div className="cart-page-container">
+          {cartList.length === 0 ? <EmptyCart /> : <CartItems />}
+        </div>
+      </section>
+    </CommonLayout>
+  )
+}
+
+export default Cart
